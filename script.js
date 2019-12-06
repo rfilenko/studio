b2c502714a0dfaecac7d2489b4178eae8971a3ef
Olia Starostina <roman.filenko@gmail.com>
2019-12-06 19:22:23 +0100

    Scrollin between section, arrowUp functionality

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 //footer current year
-let currYear = new Date().getFullYear();
-let currYearEl = document.querySelector(".current-year");
+const currYear = new Date().getFullYear();
+const currYearEl = document.querySelector(".current-year");
 currYearEl.innerHTML = currYear;
 
 // scroll
-
-let arrow = document.querySelector(".arrow-icon");
+const arrow = document.querySelector(".arrow-icon");
+const secondSection = document.querySelector("#second");
 const handleScroll = () => {
-  let secondSection = document.querySelector("#second");
   //smooth scroll into secondSection
   secondSection.scrollIntoView({ behavior: "smooth", block: "start" });
 };
 arrow.addEventListener("click", () => {
   handleScroll();
+});
+
+// scroll up
+const arrowUp = document.querySelector(".arrow-up");
+
+//show arrowUp on navLink click
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach((el) => {
+    el.addEventListener("click", () => {
+        arrowUp.classList.remove("hidden");
+      });
+});
+arrowUp.addEventListener("click", () => {
+  document
+    .getElementById("first")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+    arrowUp.classList.add('hidden')
 });
