f9ad6e9c63992d08b3d556dc77f08875e42a03c9
Olia Starostina <roman.filenko@gmail.com>
2019-12-06 18:09:44 +0100

    smooth scroll func

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-
 //footer current year
 let currYear = new Date().getFullYear();
-let currYearEl = document.querySelector('.current-year');
+let currYearEl = document.querySelector(".current-year");
 currYearEl.innerHTML = currYear;
+
+// scroll
+
+let arrow = document.querySelector(".arrow-icon");
+const handleScroll = () => {
+  let secondSection = document.querySelector("#second");
+  //smooth scroll into secondSection
+  secondSection.scrollIntoView({ behavior: "smooth", block: "start" });
+};
+arrow.addEventListener("click", () => {
+  handleScroll();
+});
